5bc5c1ce17b04f14f8c111661dd4e7a68b992308
FimbulWinters <dakin.92@outlook.com>
2024-12-27 15:55:25 +0000

    add node to html function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,55 +1,56 @@
 import unittest
 from textnode import TextNode, TextType
 
+
 class TestTextNode(unittest.TestCase):
     def setUp(self):
         # Create some common test nodes that we'll use across multiple tests
         self.bold_node = TextNode("Bold text", TextType.Bold)
         self.italic_node = TextNode("Italic text", TextType.Italic)
-        self.link_node = TextNode("Link text", TextType.Links, "https://example.com")
-    
+        self.link_node = TextNode(
+            "Link text", TextType.Links, "https://example.com")
+
     def test_eq_identical_nodes(self):
         # Test that two nodes with the same content are equal
         node1 = TextNode("This is a text node", TextType.Bold)
         node2 = TextNode("This is a text node", TextType.Bold)
         self.assertEqual(node1, node2)
-    
+
     def test_eq_different_text(self):
         # Test that nodes with different text are not equal
-        node1 = TextNode("First text", TextType.Normal)
-        node2 = TextNode("Second text", TextType.Normal)
+        node1 = TextNode("First text", TextType.Text)
+        node2 = TextNode("Second text", TextType.Text)
         self.assertNotEqual(node1, node2)
-    
+
     def test_eq_different_types(self):
         # Test that nodes with same text but different types are not equal
         node1 = TextNode("Same text", TextType.Bold)
         node2 = TextNode("Same text", TextType.Italic)
         self.assertNotEqual(node1, node2)
-    
+
     def test_eq_different_urls(self):
         # Test that link nodes with different URLs are not equal
         node1 = TextNode("Link text", TextType.Links, "https://example.com")
         node2 = TextNode("Link text", TextType.Links, "https://other.com")
         self.assertNotEqual(node1, node2)
-    
+
     def test_repr_without_url(self):
         # Test string representation of a node without URL
-        node = TextNode("Simple text", TextType.Normal)
-        expected = "TextNode(Simple text, Normal, None)"
+        node = TextNode("Simple text", TextType.Text)
+        expected = "TextNode(Simple text, Text, None)"
         self.assertEqual(repr(node), expected)
-    
+
     def test_repr_with_url(self):
         # Test string representation of a node with URL
         node = TextNode("Link text", TextType.Links, "https://example.com")
         expected = "TextNode(Link text, Links, https://example.com)"
         self.assertEqual(repr(node), expected)
-    
+
     def test_url_optional(self):
         # Test that URL is optional for non-link nodes
         node = TextNode("Code block", TextType.Code)
         self.assertIsNone(node.url)
-    
-    
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,16 +1,19 @@
 from enum import Enum
 
+from leaf_node import LeafNode
+
 
 class TextType(Enum):
-    Normal = "Normal"
+    Text = "Text"
     Bold = "Bold"
     Italic = "Italic"
     Code = "Code"
     Links = "Links"
     Images = "Images"
 
+
 class TextNode():
-    def __init__(self, text, text_type, url = None):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -19,6 +22,23 @@ class TextNode():
         if self.text == other_node.text and self.text_type == other_node.text_type and self.url == other_node.url:
             return True
         return False
-    
+
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+    def text_node_to_html_node(self):
+        match(self.text_type):
+            case TextType.Normal:
+                return LeafNode(None, self.text)
+            case TextType.Bold:
+                return LeafNode("b", self.text)
+            case TextType.Italic:
+                return LeafNode('i', self.text)
+            case TextType.Code:
+                return LeafNode("code", self.text)
+            case TextType.Links:
+                return LeafNode("a", self.text, {"href": self.url})
+            case TextType.Images:
+                return LeafNode("img", None, {"src": self.url, "alt": self.value})
+            case _:
+                raise ValueError("No or incorrect text type given")
